a0269f66a38808cbfdb62ab58384ce9b5c99b85c
varun <varunlad5@gmail.com>
2021-09-28 00:05:48 +0530

    [varun]Fibonachi Series

--- Assignment 6/FibonacciSeries.cs
+++ Assignment 6/FibonacciSeries.cs
@@ -10,15 +10,15 @@ namespace Assignment_6
     {
         public void fibonacci()
         {
-            int n1 = 0, n2 = 1, n3, n;
+            int n1 = 0, n2 = 1, n3, n;//Fibonacci Series Starts from 0 1
             Console.Write("Enter a number up to which you want the series : ");
             n = Convert.ToInt32(Console.ReadLine());
             for (int i = 1; i <= n; i++)
             {
                 Console.WriteLine(n1);
-                n3 = n1 + n2;
-                n1 = n2;
-                n2 = n3;
+                n3 = n1 + n2; //next number is the addition of last two numbers
+                n1 = n2;//first number become next mumber
+                n2 = n3;//sum become last number
             }
             Console.ReadLine();
         }
